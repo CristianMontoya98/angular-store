63187fb816fe04c67a4bea3a4d55a05125af22f1
Christian Montoya <chrimonlo@gmail.com>
2023-02-24 00:26:04 -0500

    Product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from './models/product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-store';
+  products: Product[] = [
+  {
+    id:'1',
+    name: 'Producto 1',
+    price: 100,
+    image:'../../../assets/img/toy.jpg'
+    },
+    {
+    id:'2',
+    name: 'Producto 2',
+    price: 100,
+    image:'../../../assets/img/toy.jpg'
+    },
+    {
+    id:'3',
+    name: 'Producto 3',
+    price: 100,
+    image:'../../../assets/img/toy.jpg'
+    }
+    
+    
+  ]
 }
